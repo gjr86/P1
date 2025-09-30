1130a798f416c74fadae6c8aba82912abbd920d4
gjr86 <gjr86@rutgers.edu>
2025-09-30 12:52:50 -0400

    settings

--- mymalloc.c
+++ mymalloc.c
@@ -6,3 +6,18 @@ static union {
    char bytes[MEMLENGTH];
    double not_used;
 } heap;
+
+
+
+void* mymalloc(size_t size, char *file, int line) {
+
+   return NULL;
+}
+
+void myfree(void *ptr, char *file, int line) {
+    /*
+    mark the block pointed to by ptr as free
+    here we will coalesce free blocks if possible
+    */
+   return;
+}
